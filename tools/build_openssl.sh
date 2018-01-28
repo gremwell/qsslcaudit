4e5b7eaef4510dea54979e2dbae0f5e78fbb0f3a
Pavel Nakonechnyi <pavel@gremwell.com>
2018-01-28 17:46:52 +0100

    build_openssl.sh: support building openssl 1.1.x

--- tools/build_openssl.sh
+++ tools/build_openssl.sh
@@ -4,13 +4,23 @@
 # the resulting libraries and openssl binary are stored in the current directory.
 # build is performed in a temporary directory which is removed afterwards.
 #
+# to build openssl version 1.1.x, set variable 'useopenssl11' to "yes".
+#
 # depending on your operating system you will probably be needed to update
 # 'openssldir' variable.
 
 openssldir="/etc/openssl"
 
+useopenssl11="no"
+
 opensslver="1.0.2"
 opensslrel="i"
+
+if [ $useopenssl11 = "yes" ]; then
+    opensslver="1.1.0"
+    opensslrel="f"
+fi
+
 name="openssl-${opensslver}${opensslrel}"
 archive="$name.tar.gz"
 
@@ -32,14 +42,21 @@ tar -xzf $archive
 
 pushd $name
 
-./config enable-ssl2 enable-weak-ssl-ciphers enable-shared --prefix=/usr --openssldir=$openssldir
+./config enable-ssl2 enable-ssl3 enable-ssl3-method enable-weak-ssl-ciphers enable-shared --prefix=/usr --openssldir=$openssldir
 make depend
 make
 
-cp -a libcrypto.so{,.1.0.0} $curdir
-cp -a libssl.so{,.1.0.0} $curdir
-ln -sf libssl.so{.1.0.0,.10}
-ln -sf libcrypto.so{.1.0.0,.10}
+if [ $useopenssl11 = "yes" ]; then
+    cp -a libcrypto.so{,.1.1} $curdir
+    cp -a libssl.so{,.1.1} $curdir
+    ln -sf libssl.so{.1.1,.11}
+    ln -sf libcrypto.so{.1.1,.11}
+else
+    cp -a libcrypto.so{,.1.0.0} $curdir
+    cp -a libssl.so{,.1.0.0} $curdir
+    ln -sf libssl.so{.1.0.0,.10}
+    ln -sf libcrypto.so{.1.0.0,.10}
+fi
 cp -a apps/openssl $curdir
 
 popd
