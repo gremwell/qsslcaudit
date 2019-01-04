48004fe2760af39792379720739e088c90c59394
Pavel Nakonechnyi <pavel@gremwell.com>
2019-01-04 16:21:49 +0100

    tests/: introduce RecurrentRequests test
    This test checks that qsslcaudit properly handles multiple
    re-launches and still is able to accept incoming connections.
    
    This helped to fix an issue with occasional improper SSL context
    initialization.

---
+++ tests/tests_RecurrentRequests.cpp
@@ -0,0 +1,219 @@
+#include "test.h"
+#include "ssltests.h"
+#include "ciphers.h"
+
+#include <QCoreApplication>
+
+#ifdef UNSAFE
+#include "sslunsafesocket.h"
+#else
+#include <QSslSocket>
+#endif
+
+// This verifies how sslcaudit handles requests sent repeatedly
+
+
+// do not verify peer certificate, use TLSv1.1 and stronger protocols
+// check for proper test result code each time
+class Test01 : public Test
+{
+    Q_OBJECT
+public:
+    Test01(int times) : times(times) {}
+
+    int getId() { return 1; }
+
+    void setTestSettings()
+    {
+        testSettings.setUserCN("www.example.com");
+    }
+
+    void setSslTest() { targetTest = QString("RecurrentRequests/SslTest02_1"); sslTest = new SslTest02; }
+
+public slots:
+
+    void run()
+    {
+        XSslSocket *socket = new XSslSocket;
+        QByteArray data = QByteArray("GET / HTTP/1.0\r\n\r\n");
+
+        socket->setPeerVerifyMode(XSslSocket::VerifyNone);
+        socket->setProtocol(XSsl::TlsV1_1OrLater);
+
+        setResult(0);
+
+        for (int i = 0; i < times; i++) {
+            socket->connectToHostEncrypted("localhost", 8443);
+
+            if (!socket->waitForEncrypted()) {
+                qDebug() << socket->error();
+                qDebug() << socket->errorString();
+
+                setResult(-1);
+                printTestFailed(QString("%1 attempts failed").arg(i));
+                break;
+            } else {
+                socket->write(data);
+                socket->waitForReadyRead();
+
+                if (!waitForSslTestFinished()) {
+                    socket->disconnectFromHost();
+                    setResult(-1);
+                    printTestFailed(QString("%1 attempts failed").arg(i));
+                    break;
+                }
+
+                if ((sslTest->result() == SslTest::SSLTEST_RESULT_DATA_INTERCEPTED)
+                        && (sslTest->interceptedData() == data)) {
+                    ;
+                } else {
+                    socket->disconnectFromHost();
+                    setResult(-1);
+                    printTestFailed(QString("%1 attempts failed").arg(i));
+                    break;
+                }
+            }
+
+            socket->disconnectFromHost();
+
+            // skip the listener launch for the last time
+            if ((i != (times - 1)) && !launchSslCAudit()) {
+                setResult(-1);
+                printTestFailed(QString("%1 attempts failed").arg(i));
+            }
+        }
+
+        if (getResult() == 0)
+            printTestSucceeded();
+
+        QThread::currentThread()->quit();
+    }
+
+private:
+    int times;
+};
+
+// do verify peer certificate, use TLSv1.1 and stronger protocols
+// check for proper test result code each time
+class Test02 : public Test
+{
+    Q_OBJECT
+public:
+    Test02(int times) : times(times) {}
+
+    int getId() { return 2; }
+
+    void setTestSettings()
+    {
+        testSettings.setUserCN("www.example.com");
+    }
+
+    void setSslTest() { targetTest = QString("RecurrentRequests/SslTest02_2"); sslTest = new SslTest02; }
+
+public slots:
+
+    void run()
+    {
+        XSslSocket *socket = new XSslSocket;
+
+        socket->setPeerVerifyMode(XSslSocket::VerifyPeer);
+        socket->setProtocol(XSsl::TlsV1_1OrLater);
+
+        setResult(0);
+
+        for (int i = 0; i < times; i++) {
+            socket->connectToHostEncrypted("localhost", 8443);
+
+            if (!socket->waitForEncrypted()) {
+                if (!waitForSslTestFinished()) {
+                    socket->disconnectFromHost();
+                    setResult(-1);
+                    printTestFailed(QString("%1 attempts failed").arg(i));
+                    break;
+                } else if ((sslTest->result() == SslTest::SSLTEST_RESULT_SUCCESS)
+                           && (QString::compare(socket->errorString(),
+                                                "The host name did not match any of the valid hosts for this certificate") == 0)) {
+                    ;
+                } else {
+                    socket->disconnectFromHost();
+                    setResult(-1);
+                    printTestFailed(QString("%1 attempts failed, incorrect test result").arg(i));
+                    break;
+                }
+            } else {
+                socket->disconnectFromHost();
+                setResult(-1);
+                printTestFailed(QString("%1 attempts failed").arg(i));
+                break;
+            }
+
+            socket->disconnectFromHost();
+
+            // skip the listener launch for the last time
+            if ((i != (times - 1)) && !launchSslCAudit()) {
+                setResult(-1);
+                printTestFailed(QString("%1 attempts failed").arg(i));
+            }
+        }
+
+        if (getResult() == 0)
+            printTestSucceeded();
+
+        QThread::currentThread()->quit();
+    }
+
+private:
+    int times;
+};
+
+
+void launchTest(Test *autotest)
+{
+    WHITE(QString("launching autotest #%1").arg(autotest->getId()));
+
+    // we should call it outside of its own thread
+    autotest->prepare();
+
+    QThread *autotestThread = new QThread;
+    autotest->moveToThread(autotestThread);
+    QObject::connect(autotestThread, SIGNAL(started()), autotest, SLOT(run()));
+    QObject::connect(autotestThread, SIGNAL(finished()), autotestThread, SLOT(deleteLater()));
+
+    autotestThread->start();
+
+    autotestThread->wait();
+}
+
+int main(int argc, char *argv[])
+{
+    // we need QCoreApplication instance to initialize Qt internals
+    QCoreApplication a(argc, argv);
+    int ret = 0;
+    int times = 20;
+
+    if (argc > 1) {
+        QString arg(argv[1]);
+        bool ok;
+        int t = arg.toInt(&ok);
+        if (ok)
+            times = t;
+    }
+
+    QList<Test *> autotests = QList<Test *>()
+            << new Test01(times)
+            << new Test02(times)
+               ;
+
+    while (autotests.size() > 0) {
+        Test *test = autotests.takeFirst();
+        launchTest(test);
+        if (test->getResult() != 0) {
+            ret = -1;
+        }
+        test->deleteLater();
+    }
+
+    return ret;
+}
+
+#include "tests_RecurrentRequests.moc"
