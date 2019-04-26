c75332ae6b92ad5175e1940c80d218adc4b1cd7e
Pavel Nakonechnyi <pavel@gremwell.com>
2019-04-26 13:07:41 +0200

    port to Qt 5.12.3: sslunsafemutexpool.cpp

--- src/unsafessl/sslunsafemutexpool.cpp
+++ src/unsafessl/sslunsafemutexpool.cpp
@@ -1,8 +1,46 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the QtCore module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
 
 #include "qatomic.h"
 #include "sslunsafemutexpool_p.h"
 
-#ifndef QT_NO_THREAD
+QT_BEGIN_NAMESPACE
 
 Q_GLOBAL_STATIC_WITH_ARGS(SslUnsafeMutexPool, globalMutexPool, (QMutex::Recursive))
 
@@ -25,7 +63,7 @@ Q_GLOBAL_STATIC_WITH_ARGS(SslUnsafeMutexPool, globalMutexPool, (QMutex::Recursiv
 
     For example, consider this simple class:
 
-    \snippet code/src_corelib_thread_SslUnsafeMutexPool.cpp 0
+    \snippet code/src_corelib_thread_qmutexpool.cpp 0
 
     Adding a QMutex member to the Number class does not make sense,
     because it is so small. However, in order to ensure that access to
@@ -35,7 +73,7 @@ Q_GLOBAL_STATIC_WITH_ARGS(SslUnsafeMutexPool, globalMutexPool, (QMutex::Recursiv
     Code to calculate the square of a number would then look something
     like this:
 
-    \snippet code/src_corelib_thread_SslUnsafeMutexPool.cpp 1
+    \snippet code/src_corelib_thread_qmutexpool.cpp 1
 
     This function will safely calculate the square of a number, since
     it uses a mutex from a SslUnsafeMutexPool. The mutex is locked and
@@ -107,4 +145,4 @@ QMutex *SslUnsafeMutexPool::globalInstanceGet(const void *address)
     return globalInstance->get(address);
 }
 
-#endif // QT_NO_THREAD
+QT_END_NAMESPACE
